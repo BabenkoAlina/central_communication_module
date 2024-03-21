c5db21ad96b6d0363f1e497a3152bc0f4f6f228a
loginyuk <lohin.pn@ucu.edu.ua>
2024-03-21 17:07:09 +0200

    connected ESP32 and wrote start code for HUB

--- Arduino/receiver/receiver.ino
+++ Arduino/receiver/receiver.ino
@@ -1,48 +1,44 @@
-/* 
+/*
+ *  HUB 
  *  Receiver Side Code
+ *  For ESP32-WROOM-32
  * 
   Module SX1278 // Arduino UNO/NANO    
     Vcc         ->   3.3V
     MISO        ->   D12
     MOSI        ->   D11     
     SLCK        ->   D13
-    Nss         ->   D10
+    NSS         ->   D10
     GND         ->   GND
  */
 
 #include <SPI.h>
 #include <LoRa.h>  
-String inString = "";    // string to hold incoming charaters
-String MyMessage = ""; // Holds the complete message
+String inString = "";
+String MyMessage = "";
 
 void setup() {
   Serial.begin(9600);
   LoRa.setPins(10, 8, 9);
   while (!Serial);
   Serial.println("LoRa Receiver");
-  if (!LoRa.begin(433E6)) { // or 915E6
+  if (!LoRa.begin(433E6)) 
       Serial.println("Starting LoRa failed!");
     while (1);
   }
 }
 
 void loop() {
-  
-  // try to parse packet
   int packetSize = LoRa.parsePacket();
-//  Serial.println(packetSize);
   if (packetSize) { 
-    // read packet    
     while (LoRa.available())
     {
       int inChar = LoRa.read();
       inString += (char)inChar;
       MyMessage = inString;       
-      
     }
     inString = "";     
     LoRa.packetRssi();    
     Serial.println(MyMessage);  
   }  
-  
 }
--- Arduino/transmitter/transmitter.ino
+++ Arduino/transmitter/transmitter.ino
@@ -1,27 +1,29 @@
 /*  
- *   Transmitter side Code
+ *  Sensor
+ *  Transmitter side Code
+ *  For Arduino Nano
  * 
   Module SX1278 // Arduino UNO/NANO    
     Vcc         ->   3.3V
-    MISO        ->   D12
-    MOSI        ->   D11     
-    SLCK        ->   D13
-    Nss         ->   D10 
-    GND         ->   GND    
+    MISO        ->   D19
+    MOSI        ->   D23     
+    SLCK        ->   D18
+    NSS         ->   D2
+    GND         ->   GND
  */
+
 #include <SPI.h>
 #include <LoRa.h>
 
 void setup() {
-  
   Serial.begin(9600);
 //  (int ss, int reset, int dio0)
-  LoRa.setPins(10, 8, 9);
+  LoRa.setPins(2, 34, 35);
   while (!Serial);  
   Serial.println("LoRa Sender");
   int a =  LoRa.begin(433E6);
   Serial.println(a);
-  if (!LoRa.begin(433E6)) { // or 915E6, the MHz speed of your module
+  if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
@@ -30,10 +32,7 @@ void setup() {
 void loop() {
 String MyMessage = "Hello World, this is Electronic Clinic";
   LoRa.beginPacket();  
-  Serial.println(1);
   LoRa.print(MyMessage);
   LoRa.endPacket();
-//  Serial.println(a);
   delay(100);
-
  }
