c5806e9332f5144b341948a4f26365131f7e4851
loginyuk <lohin.pn@ucu.edu.ua>
2024-04-03 20:10:00 +0300

    update spiffs

--- Arduino/hub/hub.ino
+++ Arduino/hub/hub.ino
@@ -16,6 +16,8 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <sstream>
+
 
 #include <SPI.h>
 #include <LoRa.h>
@@ -31,7 +33,7 @@ struct SensorStruct {
 //    String name;
 //    String type;
 //    String units;
-    String hashedKey;
+    std::string hashedKey;
 };
 
 std::map<uint16_t, SensorStruct> dataSPIFFS;
@@ -68,21 +70,6 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     }
 }
 
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\r\n", path);
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
-    Serial.println("- read from file:");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
-}
-
-
 void writeFile(fs::FS &fs, const char * path, SensorStruct sensor){
     Serial.printf("Writing file: %s\r\n", path);
     File file = fs.open(path, FILE_WRITE);
@@ -90,7 +77,7 @@ void writeFile(fs::FS &fs, const char * path, SensorStruct sensor){
         Serial.println("- failed to open file for writing");
         return;
     }
-    if(file.print(sensor.hashedKey.c_str()){
+    if(file.print(sensor.hashedKey.c_str())){
         Serial.println("- file written");
     } else {
         Serial.println("- write failed");
@@ -112,14 +99,7 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     }
     file.close();
 }
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
-    if (fs.rename(path1, path2)) {
-        Serial.println("- file renamed");
-    } else {
-        Serial.println("- rename failed");
-    }
-}
+
 void deleteFile(fs::FS &fs, const char * path){
     Serial.printf("Deleting file: %s\r\n", path);
     if(fs.remove(path)){
@@ -128,60 +108,6 @@ void deleteFile(fs::FS &fs, const char * path){
         Serial.println("- delete failed");
     }
 }
-void testFileIO(fs::FS &fs, const char * path){
-    Serial.printf("Testing file I/O with %s\r\n", path);
-    static uint8_t buf[512];
-    size_t len = 0;
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    size_t i;
-    Serial.print("- writing" );
-    uint32_t start = millis();
-    for(i=0; i<2048; i++){
-        if ((i & 0x001F) == 0x001F){
-          Serial.print(".");
-        }
-        file.write(buf, 512);
-    }
-    Serial.println("");
-    uint32_t end = millis() - start;
-    Serial.printf(" - %u bytes written in %u ms\r\n", 2048 * 512, end);
-    file.close();
-    file = fs.open(path);
-    start = millis();
-    end = start;
-    i = 0;
-    if(file && !file.isDirectory()){
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        Serial.print("- reading" );
-        while(len){
-            size_t toRead = len;
-            if(toRead > 512){
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            if ((i++ & 0x001F) == 0x001F){
-              Serial.print(".");
-            }
-            len -= toRead;
-        }
-        Serial.println("");
-        end = millis() - start;
-        Serial.printf("- %u bytes read in %u ms\r\n", flen, end);
-        file.close();
-    } else {
-        Serial.println("- failed to open file for reading");
-    }
-}
-
-
-
-
 
 void readFiles(fs::FS &fs, File root) {
     File file = root.openNextFile();
@@ -214,20 +140,22 @@ void readFiles(fs::FS &fs, File root) {
 void parseInfoMessage(const char* message, uint16_t addressFrom) {
     std::stringstream ss(message);
     std::string token;
-    SensorInfo info;
+    SensorStruct info;
 
     // Read sensor name
     std::getline(ss, token, ',');
-    info.sensorName = token;
+
+    info.hashedKey = token;
 
     // Add to sensor map
-    sensorMap[addressFrom] = info;
+    dataSPIFFS[addressFrom] = info;
 
     // Write to file in SPIFFS (assuming writeFile and appendFile functions are correctly implemented)
     String path = "/" + String(addressFrom);
     writeFile(SPIFFS, path.c_str(), info);
     appendFile(SPIFFS, path.c_str(), "\n");
 }
+
 
 void setup() {
   Serial.begin(9600);
