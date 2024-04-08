6c1b62e1028f89b95ca80fcd1416b775f1cdbb23
loginyuk <lohin.pn@ucu.edu.ua>
2024-04-08 14:27:58 +0300

    add pairing

--- Arduino/hub/hub.ino
+++ Arduino/hub/hub.ino
@@ -21,6 +21,24 @@
 #include <string>
 #include <sstream>
 
+#define SERVER_ADDRESS 0x00
+
+//[1 byte] - header (1 - request for pairing, 2 - response for pairing, 3-pairing procedure, 128-encrypted hello, 255-message)
+#define HEADER_PAIRING_REQUEST 1
+#define HEADER_PAIRING_RESPONSE 2
+#define HEADER_PAIRING_PROCEDURE 3
+#define HEADER_ENCRYPTED_HELLO 128
+#define INFORMAL_MESSAGE 160
+#define HEADER_MESSAGE 255
+
+struct SensorStruct {
+//    String name;
+//    String type;
+//    String units;
+    std::string hashedKey;
+};
+std::map<uint16_t, std::string> dataSPIFFS; // Use address as key
+
 struct Packet {
     uint8_t magicByte;
     uint8_t header;
@@ -29,26 +47,245 @@ struct Packet {
     char message[58];
 };
 
-std::map<uint16_t, std::string> dataSPIFFS; // Use address as key
+uint8_t privateKeyHub[66];
+uint8_t publicKeyHub[132];
+
+void createPacket(Packet& packet, uint8_t header, uint16_t addressTo, uint16_t addressFrom, const char* message) {
+    packet.magicByte = 50; // Example magic byte
+    packet.header = header;
+    packet.addressTo = addressTo;
+    packet.addressFrom = addressFrom;
+    strncpy(packet.message, message, 57);
+    packet.message[57] = '\0'; // Ensure null-terminated string
+}
 
 void parsePacket(const uint8_t* buffer, Packet& packet) {
     memcpy(&packet, buffer, sizeof(Packet));
 }
 
+
+
 void handlePairing(Packet& packet) {
-    Serial.print("Received pairing message from ");
-    Serial.print(packet.addressFrom);
-    Serial.println(": Pairing successful");
+    // first step
+    // send public key to sensor
+    if (packet.header == HEADER_PAIRING_REQUEST) {
+        Serial.print("Received pairing request from ");
+        Serial.println(packet.addressFrom);
 
-    // Assuming dataSPIFFS is a map of address to hashed key
-    std::string hashedKey(packet.message);
-    dataSPIFFS[packet.addressFrom] = hashedKey;
+        // generate public and private keys
+        // maybe need convert key to string
+        ...
 
-    Serial.println("Sensor added to SPIFFS.");
+        // send public key to sensor
+        Packet responsePacket;
+        createPacket(responsePacket, HEADER_PAIRING_RESPONSE, packet.addressFrom, SERVER_ADDRESS, publicKeyHub);
+        uint8_t buffer[sizeof(Packet)];
+        memcpy(buffer, &responsePacket, sizeof(Packet));
+        LoRa.beginPacket();
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            LoRa.write(buffer[i]);
+        }
+        LoRa.endPacket();
+        delay(2000); // Wait for response 2 seconds
+    }
+
+    // second step
+    // receive public key from sensor
+    int packetSize = LoRa.parsePacket();
+    if (packetSize == sizeof(Packet)) {
+        Packet receivedPacket;
+        uint8_t buffer[sizeof(Packet)];
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            buffer[i] = LoRa.read();
+        }
+        parsePacket(buffer, receivedPacket);
+        if (receivedPacket.header == HEADER_PAIRING_PROCEDURE) {
+            Serial.print("Received pairing response from ");
+            Serial.println(receivedPacket.addressFrom);
+
+            // save public key
+            ...
+            // combine keys and creating hash
+            ...
+
+            // send encrypted hello
+            Packet pairingPacket;
+            createPacket(pairingPacket, HEADER_ENCRYPTED_HELLO, receivedPacket.addressFrom, SERVER_ADDRESS, "Hello");
+            uint8_t buffer[sizeof(Packet)];
+            memcpy(buffer, &pairingPacket, sizeof(Packet));
+            LoRa.beginPacket();
+            for (int i = 0; i < sizeof(Packet); ++i) {
+                LoRa.write(buffer[i]);
+            }
+            LoRa.endPacket();
+            delay(2000); // Wait for response 2 seconds
+        }
+    }
+
+    // third step
+    // receive encrypted hello
+    int packetSize = LoRa.parsePacket();
+    if (packetSize == sizeof(Packet)) {
+        Packet receivedPacket;
+        uint8_t buffer[sizeof(Packet)];
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            buffer[i] = LoRa.read();
+        }
+        parsePacket(buffer, receivedPacket);
+        // maybe need some check
+        if (receivedPacket.header == HEADER_ENCRYPTED_HELLO) {
+            Serial.print("Received encrypted hello from ");
+            Serial.println(receivedPacket.addressFrom);
+            Serial.print("Message: ");
+            Serial.println(receivedPacket.message);
+        }
+
+        // write to SPIFFS
+        SensorStruct info;
+        info.hashedKey = hashedKey; // got and store earlier // Natalia part
+        dataSPIFFS[] = info;
+
+        // Write to file in SPIFFS (assuming writeFile and appendFile functions are correctly implemented)
+        String path = "/" + String(receivedPacket.addressFrom);
+        writeFile(SPIFFS, path.c_str(), info);
+        appendFile(SPIFFS, path.c_str(), "\n");
+    }
+
+    // then add sensor option
+    // got from sensor with header 160
+
 }
+
+
+
+
+
+
+
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, SensorStruct sensor){
+    Serial.printf("Writing file: %s\r\n", path);
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(sensor.hashedKey.c_str())){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+void readFiles(fs::FS &fs, File root) {
+    File file = root.openNextFile();
+    while (file) {
+        String path = file.name();
+        Serial.printf("Reading file: %s\r\n", path.c_str());
+
+        if (file.isDirectory()) {
+            Serial.println("- Skipping directory");
+        } else {
+            // Read address from file name
+            String fileName = file.name();
+            int lastIndex = fileName.lastIndexOf('/');
+            String addressString = fileName.substring(lastIndex + 1);
+            uint16_t address = addressString.toInt();
+
+            SensorStruct sensor;
+            sensor.hashedKey = file.readStringUntil('\n').c_str();
+            Serial.print("Address: ");
+            Serial.println(address);
+            Serial.print("Hashed key: ");
+            Serial.println(sensor.hashedKey.c_str());
+
+            dataSPIFFS[address] = sensor;
+        }
+
+        file = root.openNextFile();
+    }
+}
+void parseInfoMessage(const char* message, uint16_t addressFrom) {
+    std::stringstream ss(message);
+    std::string token;
+    SensorStruct info;
+
+    // Read sensor name
+    std::getline(ss, token, ',');
+
+    info.hashedKey = token;
+
+    // Add to sensor map
+    dataSPIFFS[addressFrom] = info;
+
+    // Write to file in SPIFFS (assuming writeFile and appendFile functions are correctly implemented)
+    String path = "/" + String(addressFrom);
+    writeFile(SPIFFS, path.c_str(), info);
+    appendFile(SPIFFS, path.c_str(), "\n");
+}
+
+
+
+
+
 
 void setup() {
     Serial.begin(9600);
+//  (int ss, int reset, int dio0)
     LoRa.setPins(2, 32, 33);
     while (!Serial);
     Serial.println("ESP32 Hub");
@@ -61,6 +298,10 @@ void setup() {
         Serial.println("An Error has occurred while mounting SPIFFS");
         return;
     }
+    listDir(SPIFFS, "/", 0);
+    File root = SPIFFS.open("/");
+    readFiles(SPIFFS, root);
+    root.close();
 }
 
 void loop() {
@@ -72,13 +313,19 @@ void loop() {
             buffer[i] = LoRa.read();
         }
         parsePacket(buffer, receivedPacket);
-        if (receivedPacket.magicByte == 50) { // Check if it's our protocol
-            if (receivedPacket.header == 1) { // Pairing header
-                handlePairing(receivedPacket);
-            } else {
-                Serial.print("Received message: ");
-                Serial.println(receivedPacket.message);
+        // check if my address or broadcast
+        if (receivedPacket.addressTo == SERVER_ADDRESS || receivedPacket.addressTo == 0xFFFF) {
+            if (receivedPacket.magicByte == 50) { // Check if it's our protocol
+                if (receivedPacket.header == HEADER_PAIRING_REQUEST) { // Pairing header
+                    handlePairing(receivedPacket);
+                } else {
+                    Serial.print("Received message: ");
+                    Serial.println(receivedPacket.message);
+                    Serial.print("From: ");
+                    Serial.println(receivedPacket.addressFrom);
+                }
             }
         }
+
     }
 }
--- Arduino/sensor/sensor.ino
+++ Arduino/sensor/sensor.ino
@@ -14,16 +14,29 @@
 #include <LoRa.h>
 #include <EEPROM.h>
 
-#define RECEIVER_ADDRESS 0x01
+#define SENSOR_ADDRESS 0x01
 
 #define IS_PAIRED_ADDRESS 0
 #define SERVER_ADDRESS_ADDRESS 1
 #define SECRET_HASH_ADDRESS 3
+
 #define notPaired 32
+#define Paired 64
+
+#define HEADER_PAIRING_REQUEST 1
+#define HEADER_PAIRING_RESPONSE 2
+#define HEADER_PAIRING_PROCEDURE 3
+#define HEADER_ENCRYPTED_HELLO 128
+#define INFORMAL_MESSAGE 160
+#define HEADER_MESSAGE 255
+
 
 uint16_t serverAddress;
 byte secretHashBytes[32];
 byte isPaired;
+
+uint8_t privateKeySensor[66];
+uint8_t publicKeySensor[132];
 
 int gpioPin = 1;
 
@@ -44,29 +57,130 @@ void createPacket(Packet& packet, uint8_t header, uint16_t addressTo, uint16_t a
     packet.message[57] = '\0'; // Ensure null-terminated string
 }
 
-void pairing() {
-    uint16_t serverAddress = 0xABCD; // Example server address
-    byte secretHash[32];
+void parsePacket(const uint8_t* buffer, Packet& packet) {
+    memcpy(&packet, buffer, sizeof(Packet));
+}
+
+
+
+void readEEPROM(){
+    serverAddress = EEPROM.read(SERVER_ADDRESS_ADDRESS) << 8 | EEPROM.read(SERVER_ADDRESS_ADDRESS + 1);
+    Serial.print("Server Address: ");
+    Serial.println(serverAddress);
+
+    Serial.println("Secret Hash:");
+    secretHashBytes[32];
     for (int i = 0; i < 32; i++) {
-        secretHash[i] = (byte)i; // Just an example hash
+        secretHashBytes[i] = EEPROM.read(SECRET_HASH_ADDRESS + i);
+        Serial.print(secretHashBytes[i], HEX);
     }
+    Serial.println();
+}
+
+
+void handlePairing() {
+    // first step
+    // send pairing request
+
+    // придумати де взяти адресу сервера!!!!!!!!!
+    Packet packet;
+    createPacket(packet, HEADER_PAIRING_REQUEST, serverAddress, SENSOR_ADDRESS, "Requesting pairing");
+    LoRa.beginPacket();
+    LoRa.write((uint8_t*)&packet, sizeof(Packet));
+    LoRa.endPacket();
+    delay(2000); // Wait for response 2 seconds
+
+
+    // second step
+    // pair procedure
+
+    // get public key from server
+    int packetSize = LoRa.parsePacket();
+    if (packetSize == sizeof(Packet)) {
+        Packet receivedPacket;
+        uint8_t buffer[sizeof(Packet)];
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            buffer[i] = LoRa.read();
+        }
+        parsePacket(buffer, receivedPacket);
+        // check if my address or broadcast
+        if (receivedPacket.addressTo == SENSOR_ADDRESS) {
+            if (receivedPacket.magicByte == 50) { // Check if it's our protocol
+                if (receivedPacket.header == HEADER_PAIRING_RESPONSE) { // Pairing header
+                    receivedPublicKey = receivedPacket.message;
+                    Serial.println("Received public key from server");
+                    Serial.println(receivedPublicKey);
+
+                    // generate public and private keys
+                    // maybe need convert key to string
+                    ...
+
+                    // send public key to server
+                    Packet responsePacket;
+                    createPacket(responsePacket, HEADER_PAIRING_PROCEDURE, serverAddress, SENSOR_ADDRESS,
+                                 PublicKeySensor);
+                    uint8_t buffer[sizeof(Packet)];
+                    memcpy(buffer, &responsePacket, sizeof(Packet));
+                    LoRa.beginPacket();
+                    for (int i = 0; i < sizeof(Packet); ++i) {
+                        LoRa.write(buffer[i]);
+                    }
+                    LoRa.endPacket();
+                    delay(2000); // Wait for response 2 seconds
+
+                    // combine keys and creating hash
+                    ...
+                }
+            }
+        }
+    }
+
+    // third step
+    // receive encrypted hello
+    int packetSize = LoRa.parsePacket();
+    if (packetSize == sizeof(Packet)) {
+        Packet receivedPacket;
+        uint8_t buffer[sizeof(Packet)];
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            buffer[i] = LoRa.read();
+        }
+        parsePacket(buffer, receivedPacket);
+        // maybe need some check
+        if (receivedPacket.header == HEADER_ENCRYPTED_HELLO) {
+            Serial.print("Received encrypted hello from ");
+            Serial.println(receivedPacket.addressFrom);
+            Serial.print("Message: ");
+            Serial.println(receivedPacket.message);
+        }
+
+        //send encrypted response
+        Packet pairingPacket;
+        createPacket(pairingPacket, HEADER_ENCRYPTED_HELLO, receivedPacket.addressFrom, SENSOR_ADDRESS, "Hello");
+        uint8_t buffer[sizeof(Packet)];
+        memcpy(buffer, &pairingPacket, sizeof(Packet));
+        LoRa.beginPacket();
+        for (int i = 0; i < sizeof(Packet); ++i) {
+            LoRa.write(buffer[i]);
+        }
+        LoRa.endPacket();
+        delay(2000); // Wait for response 2 seconds
+    }
+    // write to EEPROM
+
+    // should be commented in the end
+    uint8_t hashedKey[32]; // we have this hash from the previous step
+    uint16_t serverAddress; // we have this address from the previous step
 
     EEPROM.write(SERVER_ADDRESS_ADDRESS, serverAddress >> 8);
     EEPROM.write(SERVER_ADDRESS_ADDRESS + 1, serverAddress & 0xFF);
 
     for (int i = 0; i < 32; i++) {
-        EEPROM.write(SECRET_HASH_ADDRESS + i, secretHash[i]);
+        EEPROM.write(SECRET_HASH_ADDRESS + i, hashedKey[i]);
     }
 
-    EEPROM.write(IS_PAIRED_ADDRESS, 0xFF); // 0xFF means paired
+    EEPROM.write(IS_PAIRED_ADDRESS, Paired);
 
     Serial.println("Device paired successfully.");
-
-    Packet packet;
-    createPacket(packet, 1, serverAddress, 0x00, "Pairing successful");
-    LoRa.beginPacket();
-    LoRa.write((uint8_t*)&packet, sizeof(Packet));
-    LoRa.endPacket();
 }
 
 void setup() {
@@ -82,21 +196,35 @@ void setup() {
     isPaired = EEPROM.read(IS_PAIRED_ADDRESS);
     if (isPaired == notPaired) {
         Serial.println("I am ready for pairing!");
-        pairing();
+        handlePairing();
     } else {
-        Serial.println("Already paired. Sending message.");
+        pinMode(gpioPin, INPUT);
+        int state = digitalRead(gpioPin);
+        for (int i = 0; i < 100; ++i) {
+            if (state == HIGH) {
+                Serial.println("GPIO1 is HIGH. Read the address");
+            } else {
+                break;
+            }
+            delay(30);
+            if (i == 99) {
+                handlePairing();
+            }
+        }
+
+        readEEPROM();
     }
 }
 
 void loop() {
-    if (isPaired != notPaired) {
-        Packet packet;
-        createPacket(packet, 255, serverAddress, 0x00, "Hello World, this is Electronic Clinic");
-        LoRa.beginPacket();
-        LoRa.write((uint8_t*)&packet, sizeof(Packet));
-        LoRa.endPacket();
-        delay(1000); // Send message every second
-    }
+//    if (isPaired != notPaired) {
+    Packet packet;
+    createPacket(packet, HEADER_MESSAGE, serverAddress, SENSOR_ADDRESS, "Hello World, this is Electronic Clinic");
+    LoRa.beginPacket();
+    LoRa.write((uint8_t*)&packet, sizeof(Packet));
+    LoRa.endPacket();
+    delay(1000); // Send message every second
+//    }
 }
 
 }
